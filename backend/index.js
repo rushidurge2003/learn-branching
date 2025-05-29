16ecb9e0ac028c792db63ef349fc6c9263df41ea
rushidurge2003 <rushikeshdurge7794@gmail.com>
2025-05-30 01:35:55 +0530

    post-route-commit

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/getname/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello World! ${name} get from get request`);
+});
+
+app.post("/postname", (req, res) => {
+  const { name } = req.body;
+  res.send(`Hello World! ${name} get from post request`);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
