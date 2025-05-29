d55e348cdb98299a81d21f5620c498598a58e3b5
rushidurge2003 <rushikeshdurge7794@gmail.com>
2025-05-30 01:18:22 +0530

    index file update

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-//This is index.js file
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
