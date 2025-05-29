52152c7fdc44927ba5a551c3174b59c39e17b9c0
rushidurge2003 <rushikeshdurge7794@gmail.com>
2025-05-30 01:07:53 +0530

    db config update

---
+++ backend/dbconfig/dbconfig.js
@@ -0,0 +1,16 @@
+const mysql2 = require("mysql2");
+
+const connection = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "test",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the database");
+});
